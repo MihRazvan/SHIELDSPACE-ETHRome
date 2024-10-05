a97e50157749fe377b493cc0fdc837971082e19f
RAZVAN MIHAILESCU <RAZVAN.MIHAILESCU1996@GMAIL.COM>
2024-10-05 10:04:09 +0200

    change value to event in all contracts

---
+++ packages/secret-contract/src/state.rs
@@ -0,0 +1,27 @@
+use cosmwasm_std::{Addr, Binary};
+use secret_toolkit::storage::{Item, Keymap};
+
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+// Import Event struct from msg.rs
+use crate::msg::Event;
+
+pub static CONFIG: Item<State> = Item::new(b"config");
+// Storage for KV.
+pub static KV_MAP: Keymap<String, StorageItem> = Keymap::new(b"KV_MAP");
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct State {
+    pub gateway_address: Addr,
+    pub gateway_hash: String,
+    pub gateway_key: Binary,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct StorageItem {
+    // Value of the StorageItem  
+    pub value: Event,
+    // ViewingKey of the StorageItem to unlock the value
+    pub viewing_key: String,
+}
