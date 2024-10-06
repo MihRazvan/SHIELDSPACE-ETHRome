38793fef8a7eab239e971fda7c902285564854d1
ilge.ustun <153606956+ilge-ustun@users.noreply.github.com>
2024-10-06 06:15:10 +0200

    create button

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,23 +1,35 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import type { NextPage } from "next";
 import { Communities } from "~~/components/Communities";
 import { Hero } from "~~/components/Hero";
 import { RainbowKitCustomConnectButton } from "~~/components/scaffold-eth/RainbowKitCustomConnectButton";
+import { Button } from "~~/components/ui/Button";
 
 const Home: NextPage = () => {
+  const router = useRouter();
   return (
     <div
       className="w-full h-full bg-cover bg-center bg-no-repeat"
       style={{ backgroundImage: "url('/images/background.png')" }}
     >
-      <div className="absolute top-0 right-0">
+      <div className="absolute top-0 left-0">
         <RainbowKitCustomConnectButton />
       </div>
       <div className="flex flex-col w-full max-w-3xl mx-auto min-h-screen bg-[#FDFEFF]">
         <Hero />
         <Communities />
         <div className="text-center mb-2">Built at ETHRome 2024</div>
+      </div>
+      <div className="absolute top-5 right-5">
+        <Button
+          onClick={() => {
+            router.push("/create");
+          }}
+        >
+          Create
+        </Button>
       </div>
     </div>
   );
